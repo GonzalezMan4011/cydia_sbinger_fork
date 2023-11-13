86717cb3ff38d63b72f231ef76160a85447d2737
Cesar Gonzalez <cesargon4011@gmail.com>
2023-11-12 21:24:39 -0800

    forking from that dudes repo

---
+++ apt/methods/http.h
@@ -0,0 +1,49 @@
+// -*- mode: cpp; mode: fold -*-
+// Description								/*{{{*/// $Id: http.h,v 1.12 2002/04/18 05:09:38 jgg Exp $
+// $Id: http.h,v 1.12 2002/04/18 05:09:38 jgg Exp $
+/* ######################################################################
+
+   HTTP Acquire Method - This is the HTTP acquire method for APT.
+
+   ##################################################################### */
+									/*}}}*/
+
+#ifndef APT_HTTP_H
+#define APT_HTTP_H
+
+#include <apt-pkg/strutl.h>
+
+#include <string>
+#include <sys/time.h>
+#include <iostream>
+
+#include "server.h"
+
+using std::cout;
+using std::endl;
+
+class FileFd;
+class HttpMethod;
+class Hashes;
+
+class HttpMethod : public ServerMethod
+{
+   public:
+   virtual void SendReq(FetchItem *Itm) APT_OVERRIDE;
+
+   virtual std::unique_ptr<ServerState> CreateServerState(URI const &uri) APT_OVERRIDE;
+   virtual void RotateDNS() APT_OVERRIDE;
+   virtual DealWithHeadersResult DealWithHeaders(FetchResult &Res) APT_OVERRIDE;
+
+   protected:
+   std::string AutoDetectProxyCmd;
+
+   public:
+   friend struct HttpServerState;
+
+   int Loop();
+
+   explicit HttpMethod(std::string &&pProg);
+};
+
+#endif
